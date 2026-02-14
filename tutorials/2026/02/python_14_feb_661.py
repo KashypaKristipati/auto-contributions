cdd3cf2f1c1797709394fa8ea6327133bcea6c40
KashypaKristipati <159120622+KashypaKristipati@users.noreply.github.com>
2026-02-14 14:59:15 +0000

    Add python tutorial [Feb 14, 2026]

---
+++ tutorials/2026/02/python_14_feb_661.py
@@ -0,0 +1,6 @@
+# Heap Operations in Python
+## Introduction
+
+A heap is a specialized tree-based data structure that satisfies the heap property. In this example, we will implement basic heap operations such as insertion, deletion, and extraction of the minimum element.
+
+## Code
