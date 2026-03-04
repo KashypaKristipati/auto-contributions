d3d91cafd9597d25d9ee256858f3c402c8088498
KashypaKristipati <159120622+KashypaKristipati@users.noreply.github.com>
2026-03-04 16:55:32 +0000

    Add python tutorial [Mar 04, 2026]

---
+++ tutorials/2026/03/python_04_mar_412.py
@@ -0,0 +1,7 @@
+# Segment Tree Implementation in Python
+
+## Introduction
+
+A segment tree is a data structure used for storing information about segments or intervals in a way that allows for efficient querying and updating of the segments. This implementation provides a basic example of how to create a segment tree in Python.
+
+## Code
