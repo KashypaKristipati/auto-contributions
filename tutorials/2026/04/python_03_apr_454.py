c3ef48a08a086a7a927bed7b426a59a7d8d2d210
KashypaKristipati <159120622+KashypaKristipati@users.noreply.github.com>
2026-04-03 18:59:26 +0000

    Add python tutorial [Apr 03, 2026]

---
+++ tutorials/2026/04/python_03_apr_454.py
@@ -0,0 +1,12 @@
+# Recursive Backtracking
+==========================
+
+## Introduction
+
+Recursive backtracking is an algorithmic technique used to solve problems that involve searching a solution space by recursively exploring different paths until the goal condition is met.
+
+## Example Problem: N-Queens
+
+The n-queens problem is a classic example of a problem that can be solved using recursive backtracking. The goal is to place n queens on an nxn chessboard such that no two queens attack each other.
+
+## Implementation
